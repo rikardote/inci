84c0e377e5dcdb548efce45ffda9c2cd5e39f207
Ricardo <ricarmenta@gmail.com>
2021-07-07 16:23:36 -0700

    add task y schedule biometrico:checadas

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -13,7 +13,7 @@ class Kernel extends ConsoleKernel
      * @var array
      */
     protected $commands = [
-        // Commands\Inspire::class,
+         'App\Console\Commands\Biometrico'
     ];
 
     /**
@@ -24,7 +24,8 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')
-        //          ->hourly();
+         $schedule->command('biometrico:checadas')
+		->weekdays()
+                ->twiceDaily(1,12);
     }
 }
