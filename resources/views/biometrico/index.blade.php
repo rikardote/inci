7aadf77c73afd495d9b72a4bf29e386ef54f444a
Ricardo Fuentes <ricarmenta@gmail.com>
2021-08-16 11:17:20 -0700

    add link biometrico in dashboard

--- resources/views/biometrico/index.blade.php
+++ resources/views/biometrico/index.blade.php
@@ -9,15 +9,8 @@
         <div class="col-md-4"></div>
         <div class="col-md-6">
           <div class="row">
-            <form action="{{url('import')}}" method="post" enctype="multipart/form-data">
-              <div class="col-md-6">
-                {{csrf_field()}}
-                <input type="file" name="imported-file"/>
-              </div>
-              <div class="col-md-6">
-                  <button class="btn btn-primary" type="submit">Importar Checadas</button>
-              </div>
-            </form>
+           {{ $aviso }}
+            
           </div>
         </div>
         
