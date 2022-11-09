a45b664c6cc6c109e7c86e30b68f44b5df805264
ricardo fuentes <ricarmenta@gmail.com>
2022-11-09 10:16:14 -0900

    fix salida biometrico

--- resources/views/biometrico/show.blade.php
+++ resources/views/biometrico/show.blade.php
@@ -32,11 +32,14 @@
                         @foreach ($daterange as $date)
                             <tr>
                                 {{-- */ $entrada = check_entrada($date->format("Y-m-d"), $empleado->num_empleado) /* --}}
-                                {{-- */ $salida =  check_salida($date->format("Y-m-d"), $empleado->num_empleado) /* --}}
+                                {{-- */ $salida =  check_salida($date->format("Y-m-d"), $empleado->num_empleado, $entrada) /* --}}
                                 @if (!isweekend($date->format('Y-m-d')))
                                     <td> {{ $date->format('d/m/Y') }}</td>
-                                    <!-- <td> {!! valida_entrada($empleado->num_empleado, $date->format('Y-m-d'), $entrada) !!}</td> -->
-                                    <!-- <td> {!! valida_salida($empleado->num_empleado, $date->format('Y-m-d'), $salida, $entrada) !!}</td> -->
+                                    <!--
+                                        <td> {!! valida_entrada($empleado->num_empleado, $date->format('Y-m-d'), $entrada) !!}</td>
+                                        <td> {!! valida_salida($empleado->num_empleado, $date->format('Y-m-d'), $salida, $entrada) !!}</td>
+                                            -->
+
                                     <td>{{ $entrada }}</td>
                                     <td>{{ $salida }} </td>
                                 @endif
