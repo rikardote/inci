27f7e556eb14aaf12a2a91bc4d96edadf3824b7f
ricardo fuentes <ricarmenta@gmail.com>
2022-11-14 13:02:27 -0900

    fix pdf biometrico

--- resources/views/biometrico/show_pdf.blade.php
+++ resources/views/biometrico/show_pdf.blade.php
@@ -1,48 +1,49 @@
 ​<style>
     table {
-      font-family: arial, sans-serif;
-      border-collapse: collapse;
-      table-layout: fixed;
-      width: 100%;
-    }
-    
-    td, th {
-      border: 0px solid #dddddd;
-      padding: 8px;
-      width: 15%;
+        font-family: arial, sans-serif;
+        border-collapse: collapse;
+        table-layout: fixed;
+        width: 100%;
     }
 
+    td,
+    th {
+        border: 0px solid #dddddd;
+        padding: 8px;
+        width: 15%;
+    }
 </style>
 
-@foreach($empleados as $empleado)
+@foreach ($empleados as $empleado)
     <table>
         <thead>
             <tr>
-                <td colspan="5"  style="border:0px">{{ $empleado->num_empleado }} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
-                    {{ $empleado->father_lastname }} {{ $empleado->mother_lastname }} {{ $empleado->name }} 
+                <td colspan="5" style="border:0px">{{ $empleado->num_empleado }}
+                    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    {{ $empleado->father_lastname }} {{ $empleado->mother_lastname }} {{ $empleado->name }}
                 </td>
-                <td colspan="7" align="right"  style="border:0px">
-                   HORARIO:&nbsp;&nbsp;    {{ $empleado->horario }}
+                <td colspan="7" align="right" style="border:0px">
+                    HORARIO:&nbsp;&nbsp; {{ $empleado->horario }}
                 </td>
             </tr>
         </thead>
         <tbody>
             <tr style="border:1px solid #dddddd">
-                @foreach ( $daterange as $date)
-                        {{--*/ $entrada = check_entrada($date->format("Y-m-d"), $empleado->num_empleado) /*--}}
-                        {{--*/ $salida =  check_salida($date->format("Y-m-d"), $empleado->num_empleado) /*--}}
-                        @if(!isweekend($date->format("Y-m-d")))
-                            <td  style="border:1px solid #dddddd" align="center"> {{ getDia($date->format("Y-m-d")) }}
-                                    <h6> {{ $entrada }} -
-                                        @if($entrada != $salida) 
-                                        {{ $salida }}
-                                    </h6>
-                                @endif
-                            </td>
-                        @endif
-                @endforeach           
-            </tr>    
-        </tbody>
-    </table>
-    <br>
+                @foreach ($daterange as $date)
+                    {{-- */ $entrada = check_entrada($date->format("Y-m-d"), $empleado->num_empleado) /* --}}
+                    {{-- */ $salida =  check_salida($date->format("Y-m-d"), $empleado->num_empleado, $entrada) /* --}}
+                    @if (!isweekend($date->format('Y-m-d')))
+                        <td style="border:1px solid #dddddd" align="center"> {{ getDia($date->format('Y-m-d')) }}
+                            <h6> {{ $entrada }} -
+                                @if ($entrada != $salida)
+                                    {{ $salida }}
+                            </h6>
+                    @endif
+                    </td>
+                @endif
+@endforeach
+</tr>
+</tbody>
+</table>
+<br>
 @endforeach
