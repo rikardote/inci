ee6ae721bd255011f3e15e7e4a6842262209646f
ricardo fuentes <ricarmenta@gmail.com>
2023-03-14 09:11:50 -0800

    multiple schedules

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,13 +27,12 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-         $schedule->command('biometrico:checadas')
-		->weekdays()
-                ->twiceDaily(11,23);
+        $schedule = new Schedule();
+        $schedule->command('biometrico:checadas')
+		    ->weekdays()->twiceDaily(11,23)->withoutOverlapping();
 
+        $schedule = new Schedule();
         $schedule->command('biometrico5dic:checadas')
-		->weekdays()
-                ->dailyAt('23:00')
-                ->twiceDaily(9,15);
+		    ->weekdays()->dailyAt('01:00')->twiceDaily(9,15)->withoutOverlapping();
     }
 }
