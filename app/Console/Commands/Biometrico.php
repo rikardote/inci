ce7894a9fcf2f46a573a18fb8e2856313473bbd5
ricardo fuentes <ricarmenta@gmail.com>
2023-03-16 11:13:28 -0800

    BIOMETRICO GUARDADO ALL

--- app/Console/Commands/Biometrico.php
+++ app/Console/Commands/Biometrico.php
@@ -87,7 +87,7 @@ class Biometrico extends Command
         $zk2->disconnect();
 
         $progressBar = $this->output->createProgressBar(count($checadas_2));
-        $this->info('Iniciando Guardado en base de datos de checador principal comedor...'."\n");
+        $this->info("\n".'Iniciando Guardado en base de datos de checador principal comedor...'."\n");
         $progressBar->start();
             foreach($checadas_2 as $checada2){
                 $identificador2 = md5($checada2['id'].date("Y-m-d", strtotime($checada2['timestamp'])).date("H:i", strtotime($checada2['timestamp'])));
@@ -115,7 +115,7 @@ class Biometrico extends Command
         $zk3->disconnect();
 
         $progressBar = $this->output->createProgressBar(count($checadas_3));
-        $this->info('Iniciando Guardado en base de datos de checador los algodones...'."\n");
+        $this->info("\n".'Iniciando Guardado en base de datos de checador los algodones...'."\n");
         $progressBar->start();
             foreach($checadas_3 as $checada3){
                 $identificador3 = md5($checada3['id'].date("Y-m-d", strtotime($checada3['timestamp'])).date("H:i", strtotime($checada3['timestamp'])));
@@ -142,7 +142,7 @@ class Biometrico extends Command
         $zk4->disconnect();
 
         $progressBar = $this->output->createProgressBar(count($checadas_4));
-        $this->info('Iniciando Guardado en base de datos de checador los algodones...'."\n");
+        $this->info("\n".'Iniciando Guardado en base de datos de checador SAN FELIPE...'."\n");
         $progressBar->start();
             foreach($checadas_4 as $checada4){
                 $identificador4 = md5($checada4['id'].date("Y-m-d", strtotime($checada4['timestamp'])).date("H:i", strtotime($checada4['timestamp'])));
@@ -169,7 +169,7 @@ class Biometrico extends Command
         $zk5->disconnect();
 
         $progressBar = $this->output->createProgressBar(count($checadas_5));
-        $this->info('Iniciando Guardado en base de datos de checador los algodones...'."\n");
+        $this->info("\n".'Iniciando Guardado en base de datos de checador TECATE...'."\n");
         $progressBar->start();
             foreach($checadas_5 as $checada5){
                 $identificador5 = md5($checada5['id'].date("Y-m-d", strtotime($checada5['timestamp'])).date("H:i", strtotime($checada5['timestamp'])));
@@ -196,7 +196,7 @@ class Biometrico extends Command
         $zk6->disconnect();
 
         $progressBar = $this->output->createProgressBar(count($checadas_6));
-        $this->info('Iniciando Guardado en base de datos de checador los algodones...'."\n");
+        $this->info("\n".'Iniciando Guardado en base de datos de checador OTAY...'."\n");
         $progressBar->start();
             foreach($checadas_6 as $checada6){
                 $identificador6 = md5($checada6['id'].date("Y-m-d", strtotime($checada6['timestamp'])).date("H:i", strtotime($checada6['timestamp'])));
@@ -211,26 +211,64 @@ class Biometrico extends Command
             $progressBar->advance();
             }
 	    $progressBar->finish();
-/*
-        $checadas = array_merge($checadas_1, $checadas_2, $checadas_3, $checadas_4, $checadas_5, $checadas_6);
-        //$checadas = array_merge($checadas_1, $checadas_2);
-        $progressBar = $this->output->createProgressBar(count($checadas));
-        $this->info('Iniciando Guardado en base de datos...'."\n");
-        $progressBar->start();
-            foreach($checadas as $checada){
-                $identificador = md5($checada['id'].date("Y-m-d", strtotime($checada['timestamp'])).date("H:i", strtotime($checada['timestamp'])));
 
-                if(!Checada::where('identificador', $identificador)->exists()){
+         //BIOMETRICO SOTANO 5 DICIEMBRE
+
+        $zk7 = new ZKTeco("192.161.59.46");
+        $zk7->connect();
+        sleep(1);
+        $checadas_7 =  $zk7->getAttendance();
+        sleep(1);
+        $zk7->setTime(date("Y-m-d H:i:s"));
+        sleep(1);
+        $zk7->disconnect();
+        sleep(1);
+
+        $progressBar = $this->output->createProgressBar(count($checadas_7));
+        $this->info("\n".'Iniciando Guardado en base de datos de checador SOTANO 5 DE DICIEMBRE...'."\n");
+        $progressBar->start();
+            foreach($checadas_7 as $checada7){
+                $identificador7 = md5($checada7['id'].date("Y-m-d", strtotime($checada7['timestamp'])).date("H:i", strtotime($checada7['timestamp'])));
+
+                if(!Checada::where('identificador', $identificador7)->exists()){
                     DB::table('checadas')->insert([
-                        'num_empleado' => $checada['id'],
-                        'fecha'    => date("Y-m-d H:i:s", strtotime($checada['timestamp'])),
-                        'identificador' => $identificador,
+                        'num_empleado' => $checada7['id'],
+                        'fecha'    => date("Y-m-d H:i:s", strtotime($checada7['timestamp'])),
+                        'identificador' => $identificador7,
                     ]);
                 }
             $progressBar->advance();
             }
-	$progressBar->finish();
-*/
+	    $progressBar->finish();
+
+        //BIOMETRICO BUNKER 5 DICIEMBRE
+        $zk8 = new ZKTeco("192.161.59.43");
+        $zk8->connect();
+        sleep(1);
+        $checadas_8 =  $zk8->getAttendance();
+        sleep(1);
+        $zk8->setTime(date("Y-m-d H:i:s"));
+        sleep(1);
+        $zk8->disconnect();
+        sleep(1);
+
+        $progressBar = $this->output->createProgressBar(count($checadas_8));
+        $this->info("\n".'Iniciando Guardado en base de datos de checador BUNKER 5 DE DICIEMBRE...'."\n");
+        $progressBar->start();
+            foreach($checadas_8 as $checada8){
+                $identificador8 = md5($checada8['id'].date("Y-m-d", strtotime($checada8['timestamp'])).date("H:i", strtotime($checada8['timestamp'])));
+
+                if(!Checada::where('identificador', $identificador8)->exists()){
+                    DB::table('checadas')->insert([
+                        'num_empleado' => $checada8['id'],
+                        'fecha'    => date("Y-m-d H:i:s", strtotime($checada8['timestamp'])),
+                        'identificador' => $identificador8,
+                    ]);
+                }
+            $progressBar->advance();
+            }
+	    $progressBar->finish();
+
 	$this->info("\n".'Se descargaron y grabaron todas las checadas exitosamente, y se sincronizo la hora y fecha');
     }
 }
