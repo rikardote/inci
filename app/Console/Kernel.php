e9af70adfe8277f1e4e7244f2c82756ddc2e1853
ricardo fuentes <ricarmenta@gmail.com>
2023-03-17 11:00:27 -0800

    kernel biometrico 5 dic

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,6 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
          'App\Console\Commands\Biometrico',
+         'App\Console\Commands\Biometrico5dic',
          'App\Console\Commands\BiometricoTime'
     ];
 
