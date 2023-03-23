e64bf5cb16af74a519488ec74ae9939fcdd1e83f
ricardo fuentes <ricarmenta@gmail.com>
2023-03-23 13:11:58 -0800

    pruebas biometrico

--- app/Checada.php
+++ app/Checada.php
@@ -9,7 +9,7 @@ class Checada extends Model
 {
     protected $fillable = ['num_empleado','fecha','identificador'];
 
-    protected $connection = 'mysql-biometrico';
+    protected $connection = 'mysql-biometrico-pruebas';
     protected $table = 'checadas';
 
     public function employee()
