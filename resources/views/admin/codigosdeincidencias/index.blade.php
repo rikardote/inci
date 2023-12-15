add573a24f5c877a67d0974909050a3238cda4bd
ricardo <ricarmenta@gmail.com>
2023-12-15 10:54:12 -0700

    agregar txt nocturnos

--- resources/views/admin/codigosdeincidencias/index.blade.php
+++ resources/views/admin/codigosdeincidencias/index.blade.php
@@ -6,25 +6,27 @@
 <div class="supreme-container">
 <a data-url="{{ route('codigosdeincidencias.create') }}" class="load-form-modal panelColorGreen" data-toggle ="modal" data-target='#form-modal'>
     <span class="fa fa-plus-circle fa-2x" aria-hidden='true'></span>
- </a> 
+ </a>
 
 	<table class="table table-striped">
 		<thead>
 			<th>Codigo</th>
 			<th>Descripcion</th>
-			
+
 			<th>Accion</th>
 		</thead>
 		<tbody>
 		@foreach($incidencias as $incidencia)
 			<tr>
-			 <td>{{ $incidencia->code }}</td>
-			 <td>{{ $incidencia->description }}</td>
-			 
+                <td>{{ $incidencia->id }}</td>
+                <td>{{ $incidencia->code }}</td>
+                <td>{{ $incidencia->description }}</td>
+
 			 <td>
+                {{ $incidencia->id }}
 				<a data-url="{{ route('codigosdeincidencias.edit', $incidencia->code) }}" class="load-form-modal  panelColorGreen" data-toggle ="modal" data-target='#form-modal'>
 			 		<span class="fa fa-pencil-square-o fa-2x" aria-hidden='true'></span>
-  				</a> 
+  				</a>
 			 	<a href="{{route('codigosdeincidencias.destroy', $incidencia->code) }}"><span class="fa fa-times fa-2x" aria-hidden="true"></span></a>
 
 			 </td>
