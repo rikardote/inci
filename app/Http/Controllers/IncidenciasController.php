51153df562e5f6a5c927bb1be4ffeb5513217ae9
ricardo <ricarmenta@gmail.com>
2024-01-02 12:02:01 -0700

    txt normalidad

--- app/Http/Controllers/IncidenciasController.php
+++ app/Http/Controllers/IncidenciasController.php
@@ -315,12 +315,12 @@ class IncidenciasController extends Controller
                                 return response()->json('Trabajador no puede gozar mas de 1 dias de T.X.T',500);
                             }
                     }
-                    /*
+
                     if ($dias > 2 && in_array($empleado->jornada_id, $guardias)) {
                         if ($request->ajax()) {
                                 return response()->json('Trabajador no puede gozar mas de 2 dias de T.X.T',500);
                             }
-                    }*/
+                    }
 
             //    }
                 }   //termina if ($request->saltar_validacion_txt != 'true') {
