a19a210a5181ee6c557fedc09e9a98d07d8c2c5e
ricardo <ricarmenta@gmail.com>
2024-02-13 12:40:48 -0800

    ADD HEADER 2

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -121,7 +121,7 @@ class ReportsController extends Controller
    {
       $dptos = \Auth::user()->centros->pluck('id')->toArray();
       $fecha = $request->fecha_inicio;
-      $consulta_captura = "CAPTURA";
+
       if ($request->solo_medicos == true) {
         $medicosIds = ['24','25','55','56','57','58','59','60','61','62','63','64','65','66','67','68','69'];
         $incidencias = Incidencia::GetIncidenciasPorDia_Solo_Medicos($dptos, $medicosIds, fecha_ymd($fecha));
@@ -137,7 +137,7 @@ class ReportsController extends Controller
       }
       else {
         $mpdf = new mPDF('', 'Letter', 0, '', 12.7, 12.7, 14, 12.7, 8, 8);
-          $header = \View('reportes.header_diario', compact('fecha, consulta_captura'))->render();
+          $header = \View('reportes.header_diario', compact('fecha'))->render();
           $mpdf->SetFooter('Generado el: {DATE j-m-Y} |Hoja {PAGENO} de {nb}');
           $html =  \View('reportes.reporte_diario_generado', compact('incidencias'))->render();
           $pdfFilePath = 'REPORTE_DE_INCIDENCIAS_DIARIO_DEL_'.Carbon::now().'.pdf';
@@ -593,7 +593,6 @@ class ReportsController extends Controller
       $dt1 = Carbon::parse(fecha_ymd($request->fecha_inicio));
       $fecha = $request->fecha_inicio;
       $fecha_inicial = fecha_ymd($request->fecha_inicio);
-      $consulta_captura = "CONSULTA";
 
       $incidencias = Incidencia::GetIncidenciasCapturaPorDia($dptos,$fecha_inicial);
 
@@ -604,7 +603,7 @@ class ReportsController extends Controller
 
       else {
         $mpdf = new mPDF('', 'Letter', 0, '', 12.7, 12.7, 14, 12.7, 8, 8);
-          $header = \View('reportes.header_diario', compact('fecha, consulta_captura'))->render();
+          $header = \View('reportes.header_diario', compact('fecha'))->render();
           $mpdf->SetFooter('Generado el: {DATE j-m-Y} |Hoja {PAGENO} de {nb}');
           $html =  \View('reportes.reporte_diario_generado', compact('incidencias'))->render();
           $pdfFilePath = 'REPORTE_DE_INCIDENCIAS_DIARIO_DEL_'.Carbon::now().'.pdf';
