ff269dfcd087ca72228c2e8fdf16478517e34154
ricardo <ricarmenta@gmail.com>
2025-04-03 13:41:52 -0700

    qnas fxes

--- resources/views/reportes/vacaciones/show.blade.php
+++ resources/views/reportes/vacaciones/show.blade.php
@@ -9,29 +9,29 @@
 @section('content')
 
     @if(isset($incidencias))
-    
+
     <table class="table table-hover table-condensed">
         <thead>
-            
-            <th>Num Empleado</th>
+
+            <th class="text-center">Num Empleado</th>
             <th>Empleado</th>
-            <th>Codigo</th>
-            <th>Periodo</th>
-            <th>Total</th>
+            <th class="text-center">Codigo</th>
+            <th class="text-center">Periodo</th>
+            <th class="text-center">Total</th>
         </thead>
         <tbody>
         {{--*/ $tmp = "" /*--}}
         @foreach($incidencias as $incidencia)
             <tr class="no-table">
                 @if($incidencia->num_empleado == $tmp)
-                
+
                     <td></td>
                     <td></td>
                      <td align=center>{{ str_pad($incidencia->code,'2','0',STR_PAD_LEFT ) }}</td>
                      <td align=center>{{ $incidencia->periodo }}/{{ $incidencia->year }}</td>
                      <td align=center>{{ $incidencia->total}}</td>
             </tr>
-  
+
                 @else
                     <tr>
                         <td align=center>{{ $incidencia->num_empleado }}</td>
@@ -39,7 +39,7 @@
                          <td align=center>{{ str_pad($incidencia->code,'2','0',STR_PAD_LEFT ) }}</td>
                          <td align=center>{{ $incidencia->periodo }}/{{ $incidencia->year }}</td>
                          <td align=center>{{ $incidencia->total }}</td>
-                         
+
                     </tr>
                     {{--*/ $tmp = $incidencia->num_empleado /*--}}
                 @endif
